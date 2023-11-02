be36463d3f66bb4cf50428acebc1614f6b438174
iyeahh <yboravender@gmail.com>
2023-11-02 09:28:56 +0900

    conf: API KEY gitignore 파일에 추가

---
+++ WeatherForYou/Models/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  WeatherForYou
+//
+//  Created by Bora Yang on 11/1/23.
+//
+
+import Foundation
---
+++ WeatherForYou/Networking/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  WeatherForYou
+//
+//  Created by Bora Yang on 11/1/23.
+//
+
+import Foundation
